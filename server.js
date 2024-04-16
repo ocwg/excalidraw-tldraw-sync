5fe4b3a8f0af29923b7b0b198f0e52af71763d72
Orion Reed <orionreedt@gmail.com>
2024-04-16 16:21:53 +0200

    add server dependencies

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 app.use(cors());
 
 app.get("/read", (req, res) => {
